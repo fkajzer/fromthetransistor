6e83b1cea6512a72affa53d2c18ca3ec5aaf19ec
kijz <filipkajzer@gmail.com>
2024-05-18 10:45:23 +0200

    chore: ray class

---
+++ raytracer-cpp/src/one-weekend/ray.h
@@ -0,0 +1,19 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+class ray {
+public:
+  ray() {}
+  ray(const point3 &origin, const vec3 &direction)
+      : _origin(origin), _direction(direction) {}
+
+  const point3 &origin() const { return _origin; }
+  const point3 &direction() const { return _direction; }
+
+private:
+  point3 _origin;
+  vec3 _direction;
+};
+#endif
